2101cc21dbd6769313b0b8f996f55ef3750fdd31
SimonLuis1502 <lademann.simonluis@gmail.com>
2025-07-08 11:06:46 +0200

    Expand funnyNames list and update .gitignore
    Added more entries to the funnyNames array in lobbyHandling.js for greater variety. Updated .gitignore to exclude package-lock.json. Also, new dependencies 'cookie-parser' and 'jsonwebtoken' were added to package-lock.json.

--- backend/logic/lobbyHandling.js
+++ backend/logic/lobbyHandling.js
@@ -1,5 +1,20 @@
-const funnyNames = ["U-Norris", "Taylor Swift Card", "UNO DiCaprio", "Cardi Bitch", "Elon Shuffle", "Snoop Draw Two", "Oprah Skipfrey", "Keanu Draw-Reeves", "UNO Musk", "Barack Ob-Draw-ma", "Harry Skipper", "Justin Blieber", "Reverse Kardashian", "The Rock’n’Draw", "Skipney Spears", "UNOwen Wilson", "Dwayne 'The Card' Johnson", "Kim Kartdashian", "Lady Skaga", "Ed Skipran", "Will Cardsmith", "Cardashians Unite", "UNO Clarkson", "Shufflon Musk", "UNOzilla", "UNOferatu", "Beyoncé Drawoncé", "UNOli Baba und die 4 Farben", "The Weeknd Reversed", "UNOkeem Phoenix", "Cardashian West", "UNOthanos – I’ll skip half of you", "UNOmar Khaled – Another one", "UNOna Lisa", "Lil Draw-X", "Vin Unocard", "UNO Maccaroni", "UNOndo (der Vergelter)", "UNO'Neill (Shaquille Edition)", "UNO McGregor", "UNO Bieber Fever", "UNO-Tokio Drift", "UNOlectro Deluxe", "UNOzilla vs. Cardzilla", "UNO Reloaded", "UNOmander Reversal"];
-
+const funnyNames = [
+    "U-Norris", "Taylor Swift", "UNO DiCaprio", "Cardi Bitch",
+    "Elon Shuffle", "Snoop Draw Two", "Oprah Skipfrey", "Keanu Draw-Reeves",
+    "Barack Ob-Draw-ma", "Harry Skipper", "Justin Blieber", "Reverse Kardashian",
+    "The Rock’n’Draw", "Skipney Spears", "UNOwen Wilson", "Kim Kardashian",
+    "Lady Gaga", "Ed Sheeran", "Will Smith", "UNO Clarkson",
+    "Shufflon Musk", "UNOzilla", "UNOferatu", "Beyoncé Drawoncé",
+    "The Weeknd Reversed", "UNOkeem Phoenix", "Kardashian West", "UNOthanos",
+    "UNOmar Khaled", "UNOna Lisa", "Lil Draw-X", "Vin Unocard",
+    "UNO Maccaroni", "UNOndo der Vergelter", "UNO'Neill Shaq", "UNO McGregor",
+    "UNO Bieber Fever", "UNO-Tokio Drift", "UNOlectro Deluxe", "UNOzilla vs Cardzilla",
+    "UNO Reloaded", "UNOmander Reversal", "Draw-cula", "Skip Dogg",
+    "UNO-Kenobi", "Card-i B", "UNO-Licious", "Skip Skywalker",
+    "Card Solo", "Uno-Wan Kenobi", "Draw-nado", "Skip-a-licious",
+    "UNO-Mite", "Cardzilla", "The Shuffle King", "Draw Master Flash",
+    "UNO-Pocalypse", "Skip-N-Go", "Reverse Ripley"
+]
 function getRandomName() {
     return funnyNames[Math.floor(Math.random() * funnyNames.length)];
 }
